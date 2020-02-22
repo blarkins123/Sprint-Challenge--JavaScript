1f0be133410a6c7d1c783d7ac4be84e5a13a7b1a
Brianna Larkins <blarkins123@gmail.com>
2020-02-22 09:19:01 -0800

    objects2

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -19,19 +19,19 @@ const tyrannosaurus = {
 };
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 const stegosaurus = {
-  "name": "stegosaurus",
-  "diet": "herbivorous",
-  "weight": "2000kg",
-  "length": "9m",
-  "period": "Late Jurassic",
+  name: "stegosaurus",
+  diet: "herbivorous",
+  weight: "2000kg",
+  length: "9m",
+  period: "Late Jurassic",
 };
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 const velociraptor = {
-  "name": "tyrannosaurus",
-  "diet": "carnivorous",
-  "weight": "15kg",
-  "length": "1.8m",
-  "period": "Late Cretaceious",
+  name: "tyrannosaurus",
+  diet: "carnivorous",
+  weight: "15kg",
+  length: "1.8m",
+  period: "Late Cretaceious",
 };
 // Using your dinosaur objects, log answers to these questions:
 
